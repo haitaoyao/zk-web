d9701987712d512ae7e7ce40a9439beee4585340
姚海涛 <haitaoyao@yaohaitaodeMacBook-Pro.local>
2014-02-26 21:55:04 +0800

    fix for standalone

--- src/zk_web/server.clj
+++ src/zk_web/server.clj
@@ -1,7 +1,8 @@
 (ns zk-web.server
   (:require [noir.server :as server]
             [zk-web.conf :as conf]
-            ))
+            )
+  (:gen-class :main true))
 
 (server/load-views-ns 'zk-web.pages)
 
